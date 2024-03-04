d4fad242b90c18aff8e7384b4e5cb8d128ce0d4f
nethmidileka <117506445+nethmidileka@users.noreply.github.com>
2024-03-04 21:52:12 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+!pip install langchain
+from langchain import HuggingFaceHub
+from langchain import PromptTemplate, LLMChain
+
+repo_id ="tiiuae/falcon-7b-instruct"
+huggingfacehub_api_token="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"
+llm= HuggingFaceHub(huggingfacehub_api_token=huggingfacehub_api_token,
+                    repo_id=repo_id,
+                    model_kwargs={"temperature":0.7,"max_new_tokens":500})
+					
+
+template ="""Question: {question}
+             Answer: Let's give a detailed answer."""
+
+prompt =PromptTemplate(template=template, input_variables=["question"])
+
+chain=LLMChain(prompt=prompt, llm=llm)
+
+out=chain.run("Who is the father of Turing machine?")
+print(out)
+
+out=chain.run("How to create cup of milk tea?")
+print(out)
